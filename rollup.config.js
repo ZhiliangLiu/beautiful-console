c7860080c356df5870427e092d0fc4fa5abce4e6
liu <lzl0064@126.com>
2020-04-01 18:04:43 +0800

    去掉babel，直接使用typescript编译

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import typescript from 'rollup-plugin-typescript2';
 
 const extensions = [
     '.js', '.jsx', '.ts', '.tsx',
@@ -20,7 +20,6 @@ export default {
         // Allow bundling cjs modules. Rollup doesn't understand cjs
         commonjs(),
 
-        // Compile TypeScript/JavaScript files
-        babel({ extensions, include: ['src/**/*'] }),
+        typescript()
     ],
 }
