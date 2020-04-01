4322825c97d6e53666e7064b8c0859fbd5b71a74
liu <lzl0064@126.com>
2020-04-01 16:23:35 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
+const extensions = [
+    '.js', '.jsx', '.ts', '.tsx',
+];
+
+export default {
+    input: 'src/index.ts',
+    output: {
+        file: './dist/bundle.js',
+        format: 'es'
+    },
+    external: [],
+    plugins: [
+        // Allows node_modules resolution
+        resolve({ extensions }),
+
+        // Allow bundling cjs modules. Rollup doesn't understand cjs
+        commonjs(),
+
+        // Compile TypeScript/JavaScript files
+        babel({ extensions, include: ['src/**/*'] }),
+    ],
+}
